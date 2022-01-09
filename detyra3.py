d55be7e34b8dbc85a9d89c9a92dbb005a0bdac21
Doruntina Hoti <62035237+dorunt1na3@users.noreply.github.com>
2022-01-09 14:36:47 +0100

    Function for updating our data

--- detyra3.py
+++ detyra3.py
@@ -28,7 +28,7 @@ last_upload, last_download, upload_speed, down_speed = 0, 0, 0, 0
 window = tk.Tk()
 
 window.title("Monitorimi i bandwidth")  # Titulli
-window.geometry(f"{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}")  # Madhesua
+window.geometry(f"{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}")  # Madhesia
 window.resizable(width = WINDOW_RESIZEABLE, height = WINDOW_RESIZEABLE)  # Not resizable
 
 label_total_upload_header = tk.Label(text = "Totali Upload:", font = "Quicksand 12 bold")
@@ -55,3 +55,46 @@ label_download_header = tk.Label(text = "Download:", font = "Quicksand 12 bold")
 label_download_header.pack()
 label_download = tk.Label(text = "Duke kalkuluar...", font = "Quicksand 12")
 label_download.pack()
+
+#Update-imi i te dhenave
+def update():
+	global last_upload, last_download, upload_speed, down_speed
+	counter = net_io_counters()
+
+	upload = counter.bytes_sent
+	download = counter.bytes_recv
+	total = upload + download
+
+	# Kalkulimi i shpejtesise
+	if last_upload > 0:
+		if upload < last_upload:
+			upload_speed = 0
+		else:
+			upload_speed = upload - last_upload
+
+	if last_download > 0:
+		if download < last_download:
+			down_speed = 0
+		else:
+			down_speed = download - last_download
+
+	last_upload = upload
+	last_download = download
+	
+	label_total_upload["text"] = f"{size(upload)} ({upload} Bytes)"
+	label_total_download["text"] = f"{size(download)} ({download} Bytes)"
+	label_total_usage["text"] = f"{size(total)}\n"
+	
+	label_upload["text"] = size(upload_speed)
+	label_download["text"] = size(down_speed)
+	
+	label_total_upload.pack()
+	label_total_download.pack()
+	label_total_usage.pack()
+	label_upload.pack()
+	label_download.pack()
+	
+	window.after(REFRESH_DELAY, update)  
+
+window.after(REFRESH_DELAY, update)
+window.mainloop()
